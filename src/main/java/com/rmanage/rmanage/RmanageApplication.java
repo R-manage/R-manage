23ca722707e18f44b0ac6b05523bdde4307d714d
k-oyun <113766051+k-oyun@users.noreply.github.com>
2023-07-27 19:43:55 +0900

    Create project

---
+++ src/main/java/com/rmanage/rmanage/RmanageApplication.java
@@ -0,0 +1,13 @@
+package com.rmanage.rmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmanageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmanageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rmanage/rmanage/RmanageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmanage.rmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmanageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
